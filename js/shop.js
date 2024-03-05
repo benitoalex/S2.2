1d95630dfffa8e0ceb70a372a7006f8c623add0e
Mac Pro <a@posteo.es>
2024-03-05 11:25:06 +0100

    Nivell 1 Exercici 3

--- js/shop.js
+++ js/shop.js
@@ -100,8 +100,18 @@ function cleanCart() {
 
 // Exercise 3
 function calculateTotal() {
+    let total = 0;
+
+    for (let i = 0; i < cart.length; i++){
+        total += cart[i].price * cart[i].quantity;
+    }
+
+    return total;
     // Calculate total price of the cart using the "cartList" array
 }
+
+console.log("Carret", cart);
+console.log("Import", calculateTotal());
 
 // Exercise 4
 function applyPromotionsCart() {
